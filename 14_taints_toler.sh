08d79586fef9cb76da7a50f999ebb68d417c0e86
Delawar Jalat <delawar.jalat@bsigroup.com>
2025-10-12 18:28:46 +0100

    done requests limiting

--- 14_taints_toler.sh
+++ 14_taints_toler.sh
@@ -5,7 +5,13 @@
 # further scheduling control can be achieved using node selectors.
 # by applying labels to nodes and using node selectors in pods, you can control which nodes a pod can be scheduled on.
 # if the pod has the required node selector, but not the required tolerations, it will not be scheduled on the tainted node.
-# however the opposite is not true, if the pod has the required tolerations but not the required node selector, it will still be scheduled on the tainted node.
+#  if the pod has the required tolerations but not the required node selector, it will still be scheduled on the tainted node.
+# both the tolerations and node selectors must be satisfied for the pod to be scheduled on a tainted node.
+
+##summary
+# Toleration alone (no node selector): Pod can schedule on matching tainted nodes OR untainted nodes
+# Toleration + wrong node selector: Pod is blocked from that node (fails node selector check)
+# Toleration + correct node selector: Pod MUST go to that specific node
 
 k get nodes
 # kubectl get pods --all-namespaces -o wide
@@ -17,15 +23,20 @@ k taint nodes cka-cluster2-worker gpu=true:NoSchedule
 k taint node cka-cluster2-worker2 gpu=true:NoSchedule
 # tainting another node with the same taint
 
+
 k describe node cka-cluster2-worker | grep -i taint
 # checking the taints on the node
+# now all nodes are tainted
+
 
 k run nginx --image=nginx
 # creating a pod without any tolerations
 k get pods
 # checking the status of the pod
 k describe pod nginx
-# describing the pod to see if it was scheduled or not
+# describing the pod to see if it was scheduled or not, it should be in pending state because all nodes are tainted
+
+
 
 k run redis --image=redis --dry-run=client -o yaml > 14_taints_tols/redis.yaml
 # creating a pod with tolerations
@@ -34,12 +45,14 @@ k apply -f 14_taints_tols/redis.yaml
 
 k get pods -o wide
 # checking the status of the pod with tolerations, node it is scheduled on
+# it should be in running state because it has tolerations for the taint on the nodes
 
 k taint node cka-cluster2-worker gpu:NoSchedule-  
-# removing the taint from the node with key 'gpu' and effect 'NoSchedule'
+# removing the taint from one of the node with key 'gpu' and effect 'NoSchedule'
 
 k get pods -o wide
 # checking the status of the pod without tolerations again , to see if it sscheduled
+# now the nginx pod should be in running state because one node is untainted
 
 k describe pod redis
 
@@ -47,7 +60,7 @@ k run nginx-new --image=nginx --dry-run=client -o yaml > newnginx.yaml
 # creating a new pod with node selector to avoid GPU nodes
 
 k apply -f newnginx.yaml
-# applying the new pod with node selector
+# applying the new pod with node selector with nodeSelector:  -> gpu: "false"
 
 k get pods -o wide
 # checking the status of the new pod with node selector
@@ -65,3 +78,4 @@ k label node cka-cluster2-worker gpu=true
 
 k label node cka-cluster2-worker gpu=false --overwrite=true
 # overwriting the label on the first worker node to 'gpu=false'
+# now the first worker node has label 'gpu=false' and no taint so the new nginx pod should be scheduled on it
--- 16_req.sh
+++ 16_req.sh
@@ -13,22 +13,41 @@ k top node
 k create ns mem-example
 # creating a namespace for memory example
 
-touch mem-request.yaml
+touch within_limits.yaml
 
 
-k apply -f mem-request.yaml
+k apply -f within_limits.yaml
 # applying the metric server configuration. 
 # Where mempory consumed is within limits
 
 k top pod memory-demo -n mem-example
 # OOMKilled status
 
-touch mem-req2.yaml
+touch above-limits.yaml
 
 
-k apply -f mem-req2.yaml
+k apply -f above-limits.yaml
 # applying the memory request configuration
 # Where memory consumed is above limits
 
 k get pods -n mem-example
+
+k describe pod memory-demo2 -n mem-example
+
+
+k apply -f insuficient-memory.yaml
+# applying the memory request configuration
+# Where memory requested is more than available memory
+
+k describe pod memory-demo3 -n mem-example
+# Pod is in pending state
+# because the requested memory is more than available memory
+#     resources:
+    #   requests:
+    #     memory: "1000Gi" # requesting 1000Gi of memory, more than node capacity
+    #   limits:
+    #     memory: "1000Gi" # requesting 1000Gi of memory, more than node capacity
+
+k delete pod memory-demo -n mem-example
+k delete pod memory-demo2 -n mem-example
 
